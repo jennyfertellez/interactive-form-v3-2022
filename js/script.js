19a6f451af5cf392113f1ab09a8c7c45cbc57574
jennyfertellez <102021487+jennyfertellez@users.noreply.github.com>
2022-10-10 20:23:35 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,47 @@
-console.log('test');
+//console.log('test');
+//The "Name" Field
+document.getElementById("name").focus();
+
+//"Job Role" Section
+var otherJob = document.getElementById("other-job-role");
+otherJob.style.display = "none";
+
+let jobOption = document.querySelector("#title");
+    jobOption.addEventListener("change", (e) => {
+        if(e.target.value === "other") {
+            otherJob.style.display = "block";
+        } else {
+            otherJob.style.display = "none";
+        }
+    });
+
+//"T-Shirt Info" Section
+var designChosen = document.getElementById("shirt-designs");
+var shirtColor = document.getElementById("color");
+    shirtColor.disabled = true;
+var colorOption = shirtColor.children;
+
+designChosen.addEventListener('change', (e) => {
+    shirtColor.disabled = false;
+    for (let i = 0; i < colorOption.length; i ++) {
+        if (e.target.value === colorOption[i].getAttribute("data-theme")) {
+            colorOption[i].hidden = false;
+            colorOption[i].setAttribute("selected", true);
+        } else {
+            colorOption[i].hidden = true;
+            colorOption[i].removeAttribute("selected");
+        }
+    }
+});
+
+//"Register for Activities" Section
+const registration = document.getElementById("activities");
+const total = document.getElementById("activities-cost");
+let totalCost = 0;
+
+registration.addEventListener("change", (e) => {
+    var cost = e.target.getAttribute("data-cost");
+    var totalData = +cost;
+});
+console.log(cost);
+console.log(typeof cost);
